f3c1c671538a700d64705cc2b7051b563cf57103
Martin Vitabar <vitabarmartin@gmail.com>
2024-07-02 16:58:22 -0300

    adjusting form

--- scripts.js
+++
@@ -1,8 +0,0 @@
-let button = document.getElementsByClassName("faq-button");
-let open = document.getElementsByClassName("answer");
-
-function deploy(open) {
-  console.log("click");
-}
-
-button.addEventListener("click", open);
