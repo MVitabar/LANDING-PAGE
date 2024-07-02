d8abb55901fdc5021efb8942386d90e2e8401c44
Martin Vitabar <vitabarmartin@gmail.com>
2024-07-02 14:38:50 -0300

    Landing page init

---
+++ scripts.js
@@ -0,0 +1,8 @@
+let button = document.getElementsByClassName("faq-button");
+let open = document.getElementsByClassName("answer");
+
+function deploy(open) {
+  console.log("click");
+}
+
+button.addEventListener("click", open);
